cb51256aad37a5cd8a8892619d3045f559536c95
BrucePB <33796338+BrucePB@users.noreply.github.com>
2018-01-09 17:12:48 +0800

    004

--- demo/004.py
+++ demo/004.py
@@ -1,19 +1,21 @@
-#!bin/python3
+#!/bin/python3
 
 import re
 def get_word_frequencies(file_name):
     dic = {}
-    txt = open(file_name,'r').read().splitlines()
-    no_flag = 0
+    txt = open(file_name, 'r').read().splitlines()
+    no_flag=0
     for line in txt:
-        line = re.sub(r'[.?!,""/\W]',' ',line)    #要替换的标点符号,英文字符可能出现的
+        line = re.sub(r'[.?!,""/\W]', ' ', line)
         for word in line.split():
-            #当字符为纯数字的时候,跳过不统计
+
             if word.isdigit():
-                pass
+                    pass
+
             else:
-                dic.setdefault(word.lower(),0)  #不区分大小写
-                dic[word.lower()] += 1
-    print(dic)
-if __name__=='__main__':
-    get_word_frequencies("WhatisPyth)on.txt"
+                 dic.setdefault(word.lower(), 0)
+                 dic[word.lower()] += 1
+    print (dic)
+
+if __name__ == '__main__':
+        get_word_frequencies("WhatisPython.txt")
